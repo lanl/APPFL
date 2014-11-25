243b5734e04fbbd96d68d8b318b4e67a9045ec6a
dpx <dpx@lanl.gov>
2014-11-25 14:19:49 -0700

    more work on parser

--- parser.hs
+++ parser.hs
@@ -1,6 +1,12 @@
+-- From "Higher-Order Functions for Parsing" Graham Hutton
+
 import Data.Char 
 
+-- 2 Parsing Using Combinators
+
 type Parser b a = [b] -> [(a,[b])]
+
+-- 2.1 Primitive parsers
 
 --succeed :: a -> b -> [(a,b)]
 --succeed :: a -> [b] -> [(a,[b])]
@@ -20,11 +26,16 @@ satisfy p (x:xs)
 literal :: Eq b => b -> Parser b b
 literal x = satisfy (==x)
 
+-- 2.2 Combinators
+
 alt :: Parser b a -> Parser b a -> Parser b a
 (p1 `alt` p2) inp = p1 inp ++ p2 inp
 
+-- then
 sequ :: Parser b a -> Parser b c -> Parser b (a,c)
 (p1 `sequ` p2) inp = [((v1,v2),out2) | (v1,out1) <- p1 inp, (v2,out2) <- p2 out1]
+
+-- 2.3 Manipulationg values
 
 using :: Parser b a -> (a -> c) -> Parser b c
 (p `using` f) inp = [(f v,out) | (v,out) <- p inp]
@@ -47,4 +58,16 @@ word = some (satisfy isAlpha)
 string :: Eq b => [b] -> Parser b [b]
 string [] = succeed []
 string (x:xs) = (literal x `sequ` string xs) `using` cons
+
+xthen :: Parser b a -> Parser b c -> Parser b c
+p1 `xthen` p2 = (p1 `sequ` p2) `using` snd
+
+thenx :: Parser b a -> Parser b c -> Parser b a
+p1 `thenx` p2 = (p1 `sequ` p2) `using` fst
+
+-- return
+ret :: Parser b a -> c -> Parser b c
+p `ret` v = p `using` (const v) 
+
+-- 2.4 Example
 
