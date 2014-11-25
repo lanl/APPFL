d3471b4a40af5177f3a179b1e6dd3044bbdf4142
dpx <dpx@lanl.gov>
2014-11-25 10:54:10 -0700

    Kei's fix + some stg tests

--- ministg-0.3/src/Ministg/Utils.hs
+++ ministg-0.3/src/Ministg/Utils.hs
@@ -14,9 +14,11 @@ module Ministg.Utils where
 import Control.Monad 
    ( liftM )
 
+import Control.Exception(catch,IOException)
+
 safeReadFile :: FilePath -> IO (Either String String) 
 safeReadFile file
-   = catch (rightReadFile file) $ \error -> return $ Left $ show error 
+   = catch (rightReadFile file) ( \error -> return $ Left $ show (error :: IOException))
    where
    rightReadFile :: FilePath -> IO (Either String String)
    rightReadFile file = liftM Right $ readFile file
