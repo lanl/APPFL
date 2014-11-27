7877bfd648163b55286b7ee28b4fa846164f9f03
Dean Prichard <dean.prichard@gmail.com>
2014-11-27 13:52:44 -0700

    work on Miranda parser

--- parser4.hs
+++ parser4.hs
@@ -3,9 +3,57 @@
 
 import Data.Char
 
+-- 2 Parsing Using Combinators
+
 type Parser b a = [b] -> [(a,[b])]
 
+-- 2.1 Primitive parsers
+
+succeed :: a -> Parser b a
+succeed v inp = [(v,inp)]
+
+failure :: Parser b a
+failure inp = []
+
+literal :: Eq b => b -> Parser (Pos b) b
+literal x = satisfy (==x)
+
+-- 2.2  Combinators
+alt :: Parser b a -> Parser b a -> Parser b a
+(p1 `alt` p2) inp = p1 inp ++ p2 inp
+
+-- then
+sequ :: Parser b a -> Parser b c -> Parser b (a,c)
+(p1 `sequ` p2) inp = [((v1,v2),out2) | (v1,out1) <- p1 inp, (v2,out2) <- p2 out1]
+
+-- 2.3 Manipulating values
+
+using :: Parser b a -> (a -> c) -> Parser b c
+(p `using` f) inp = [(f v,out) | (v,out) <- p inp]
+
+cons :: (a, [a]) -> [a]
+cons (x,xs) = x:xs
+
+many :: Parser b a -> Parser b [a]
+many p = ((p `sequ` many p) `using` cons ) `alt` (succeed [])
+
+some :: Parser b a -> Parser b [a]
+some p = (p `sequ` many p) `using` cons
+
+-- 3.1 Free-format input
+any' :: (b -> Parser a c) -> [b] -> Parser a c
+any' p = foldr (alt . p) failure
+
+-- 3.3 The offside combinator
+
 type Pos b = (b, (Int,Int))
+
+satisfy :: (b->Bool) -> Parser (Pos b) b
+satisfy p [] = failure []
+satisfy p (x:xs)
+        | p a = succeed a xs
+        | otherwise = failure xs
+          where (a,(r,c)) = x
 
 -- 4.1 Example language
 
@@ -28,4 +76,10 @@ tok :: Parser (Pos Char) [Char] -> Tag -> Parser (Pos Char) (Pos Token)
 (p `tok` t) inp = [(((t,xs),(r,c)),out) | (xs,out) <- p inp]
 	          where (x,(r,c)) = head inp
 
-
+lex :: [(Parser (Pos Char) [Char],Tag)] -> Parser (Pos Char) [Pos Token]
+lex = many . (foldr op failure)
+	where (p,t) `op` xs = (p `tok` t) `alt` xs	
+{-
+lexer :: Parser (Pos Char) [Pos Token]
+lexer = lex [(some (any' literal " \t\n"), Junk),
+-}
