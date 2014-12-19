12c83ac7d721998cc9ffc74127d626e730700af8
Dean Prichard <dean.prichard@gmail.com>
2014-12-19 10:12:46 -0700

    finish ch12

--- hutton/ch12.hs
+++ hutton/ch12.hs
@@ -26,4 +26,22 @@ fib1000 = last $ takeWhile (<1000) fibs
 
 fib1000' :: Integer
 fib1000' = last $ fst $ span (<1000) fibs
+
+--Ex 6
+
+data Tree a = Leaf | Node (Tree a) a (Tree a) deriving(Show)
+
+repeatT :: a -> Tree a
+repeatT x = Node (repeatT x) x (repeatT x)
+
+takeT :: Int -> Tree a -> Tree a
+takeT 0 _ = Leaf
+takeT 1 (Node l x r) = Node Leaf x Leaf
+takeT 2 (Node l x r) = Node (takeT 1 l) x Leaf
+takeT 3 (Node l x r) = Node (takeT 1 l) x (takeT 1 r)
+takeT n (Node l x r) = Node (takeT half l) x (takeT (n-1-half) r)
+                       where half =  n `div` 2                       
+
+replicateT :: Int -> a -> Tree a 
+replicateT n x = takeT n (repeatT x)
  
