7d91226049e399dc27d5e734250e40e5e387dc84
Dean Prichard <dpx@lanl.gov>
2015-03-13 10:24:44 -0600

    rename

--- runtime/gc.c
+++ runtime/gc.c
@@ -5,27 +5,27 @@
 #include "gc.h"
 #include "stg.h"
 
-void *to=NULL, *from=NULL;
-void *scan=NULL, *free=NULL;
+void *toPtr=NULL, *fromPtr=NULL;
+void *scanPtr=NULL, *freePtr=NULL;
 
 void initGc(void) {
   assert(stgHeap && "heap not defined"); 
-  from = stgHeap;
-  to = (char *)stgHeap + stgHeapSize/2;
-  scan = to;
-  free = to;
+  fromPtr = stgHeap;
+  toPtr = (char *)stgHeap + stgHeapSize/2;
+  scanPtr = toPtr;
+  freePtr = toPtr;
 }
 
 void swapPtrs(void) {
-  assert( scan == free && "swapPtrs called when gc not finished");
-  stgHeap = to;
-  stgHP = free;
-  to = from;
-  from = stgHP;
+  assert( scanPtr == freePtr && "swapPtrs called when gc not finished");
+  stgHeap = toPtr;
+  stgHP = freePtr;
+  toPtr = fromPtr;
+  fromPtr = stgHP;
 }
 
 bool isFrom(void *p) {
-  return (p >= from && (char *)p < (char *)from + stgHeapSize/2); 
+  return (p >= fromPtr && (char *)p < (char *)fromPtr + stgHeapSize/2); 
 }
 
 
