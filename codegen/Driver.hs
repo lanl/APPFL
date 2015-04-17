7c7bff040a7df5c84f3a9b24ebd2abae24621769
Dean Prichard <dpx@lanl.gov>
2015-04-16 20:22:15 -0600

    minor driver changes

--- codegen/Driver.hs
+++ codegen/Driver.hs
@@ -8,7 +8,6 @@ module Driver (
 ) where
 
 import           Analysis
-import           Boilerplate
 import           CodeGen
 import           ConMap2
 import           InfoTab
@@ -18,6 +17,25 @@ import           Rename
 import           SetFVs
 
 import           Data.List
+header :: String
+header = "#include \"stg_header.h\"\n"
+        
+footer :: String
+footer = "\nDEFUN0(start) {\n" ++
+         "  registerSHOs();\n" ++
+         "  stgPushCont(showResultCont);\n" ++
+         "  STGEVAL(((PtrOrLiteral){.argType = HEAPOBJ, .op = &sho_main}));\n" ++
+         "  STGRETURN0();\n" ++
+         "  ENDFUN;\n" ++
+         "}\n\n" ++
+         "int main (int argc, char **argv) {\n" ++
+         "  initStg();\n" ++
+         "  initCmm();\n" ++
+         "  initGc();\n" ++
+         "  CALL0_0(start);\n" ++
+         "  showStgHeap();\n" ++
+         "  return 0;\n" ++
+         "}\n\n"
 
 -- nameDefs
 --  :: [([Char], Obj)] ->
