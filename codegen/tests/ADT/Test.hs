52f7938574ae3a2515bbbe03d549e5ecbe517f74
Dean Prichard <dean.prichard@gmail.com>
2015-04-26 15:39:50 -0600

    missed some files

---
+++ codegen/tests/ADT/Test.hs
@@ -0,0 +1,20 @@
+module ADT.Test where
+import           ADT
+import           Parser
+
+import           Test.Tasty
+import           Test.Tasty.Golden
+
+import           Data.ByteString.Lazy
+import           Data.String
+import qualified System.IO            as IO
+
+unitTests :: TestTree
+unitTests = testGroup "ADT Unit tests"
+    [ goldenVsString "adt" "tests/ADT/adt.gold" adt
+    ]
+               
+adt:: IO ByteString
+adt = do
+        input <- IO.readFile "tests/ADT/adt.stg"
+        return $ fromString $ show $ updatedata $ parse input
