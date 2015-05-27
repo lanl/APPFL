7422892b62b2bf32738f952d77fc07cd5f6a9f9d
Dean Prichard <dpx@lanl.gov>
2015-05-26 18:17:17 -0600

    driver work in progress

--- codegen/ADT.hs
+++ codegen/ADT.hs
@@ -1,18 +1,23 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE NamedFieldPuns    #-}
 
-module ADTnew (
+module ADT (
   Def(..),
+  Boxedness(..),
   TyCon(..),
   DataCon(..),
+  Atype(..),
   TyVar,
   Polytype(..),
   Monotype(..),
+  Boxedtype(..),
+  Unboxedtype(..),
   TyConParam(..),
   TyConMap,
   DataConParam(..),
   DataConMap,
-  ConMaps
+  ConMaps,
+  updateTycons
 ) where
 
 import AST
@@ -37,8 +42,7 @@ import qualified Data.Map as Map
   data unboxed \Chi# \alpha_1 .. \alpha_t =
   c_1# \tau_11 .. \tau_1a_1 | ... | c_n# \tau_n1 .. \tau_na_1
   
-  Polytype      \sigma ::=  \forall \alpha . \sigma 
-                        |   \tau
+  Polytype      \sigma ::=  \forall \alpha . \sigma | \tau
 
   Monotype      \tau   ::=  \pi | \nu
 
@@ -46,7 +50,7 @@ import qualified Data.Map as Map
                         |   \tau -> \tau             Function type
                         |   \Chi \pi_1 ... \pi_n   Parameterized boxed data type
 
-  Unboxed type  \nu     ::=  Int#
+  Unboxed type \nu     ::=  Int#
                         |   Double#
                         |   Bool#
                         |   \Chi# \pi_1 ... \pi_n   Parameterized boxed data type
@@ -55,36 +59,63 @@ import qualified Data.Map as Map
 
 
 data Def a = ObjDef (Obj a)
-           | DataDef TyCon
-             deriving(Eq,Show)
+             | DataDef TyCon
+               deriving(Eq,Show)
 
+data Boxedness = Boxed | Unboxed deriving(Eq,Show)
+ 
 -- Boxed: data \Chi \alpha_1 .. \alpha_t =
 -- c_1 \tau_11 .. \tau_1a_1 | ... | c_n \tau_n1 .. \tau_na_1  
 -- Unboxed: data unboxed \Chi# \alpha_1 .. \alpha_t =
 -- c_1# \tau_11 .. \tau_1a_1 | ... | c_n# \tau_n1 .. \tau_na_1   
-data TyCon = TyCon Bool Con [TyVar] [DataCon]
+data TyCon = TyCon Boxedness Con [TyVar] [DataCon]
              deriving(Eq,Show)
              
 -- Boxed True: c_x \tau_x1 .. \tau_xa_1 
 -- Boxed False: c_x# \tau_x1 .. \tau_xa_1  
-data DataCon = DataCon Bool Con [Monotype]
+data DataCon = DataCon Boxedness Con [Monotype]
                deriving(Eq,Show)
 
+-- \alpha                     
 type TyVar = String
 
-data Polytype = PPoly [TyVar] Monotype
+data Polytype = PPoly [TyVar] Monotype  -- uncurried forall
               | PMono Monotype
                 deriving(Eq,Show)
                 
-data Monotype = MTyVar TyVar
-              | MTyFun Monotype Monotype
-              | MTyCon Bool Con [Monotype]
+-- \tau w/ addition of Atype (see below)
+data Monotype = Mono Atype
+              | MBoxed Boxedtype
+              | MUnboxed Unboxedtype
                 deriving(Eq,Show)
+
+-- Atype is used on first pass,it is then converted to Boxed/Unboxed   
+-- this is the first def of \tau from pg 6 of the paper
+-- also atype from haskell 2010 standard
+-- https://www.haskell.org/onlinereport/haskell2010/haskellch4.html#x10-680004.2
+data Atype = ATyVar TyVar
+           | AInt
+           | ADouble
+           | AFun Atype Atype
+           | ATyCon Con [Atype]
+             deriving(Eq,Show)              
+
+-- \pi
+data Boxedtype = BTyVar TyVar
+               | BFun Monotype Monotype
+               | BTyCon Con [Boxedtype]  
+                 deriving(Eq,Show)
+-- \nu
+data Unboxedtype = UInt 
+                 | UDouble
+                 | UBool
+                 | UTyCon Con [Boxedtype] 
+                   deriving(Eq,Show)
 
 -- Type constr name to arity, tag, boxed/unboxed
 data TyConParam = TyConParam {tarity :: Int, 
                               ttag :: Int, 
-                              tboxed :: Bool,
+                              tboxed :: Boxedness,
                               tdatacons :: [Con]}
                   deriving(Eq,Show)
 
@@ -93,7 +124,7 @@ type TyConMap = Map.Map String TyConParam
 -- Data constr name to (arity, tag, boxedness, type constr name)
 data DataConParam = DataConParam {darity :: Int, 
                                   dtag :: Int, 
-                                  dboxed :: Bool, 
+                                  dboxed :: Boxedness, 
                                   dtycon :: Con} 
                     deriving(Eq,Show)
 
@@ -103,21 +134,124 @@ type ConMaps = (TyConMap, DataConMap)
          
  -- starting tycon map
 tyconmap :: TyConMap
-tyconmap = Map.insert "Bool#" (TyConParam 0 0 False ["False_h","True_h"])
-         $ Map.insert "Int_h"  (TyConParam 0 1 False [])
-         $ Map.insert "Bool"  (TyConParam 0 2 True ["B"]) -- data Bool = B Bool#
-         $ Map.insert "Int"   (TyConParam 0 3 True ["I"]) -- data Int = I Int#
-         $ Map.insert "List"  (TyConParam 1 4 True ["Nil","Cons"]) -- data List a = Nil | Cons a (List a)
+tyconmap = Map.insert "Bool#" (TyConParam 0 0 Unboxed ["False_h","True_h"])
+         $ Map.insert "Int_h"  (TyConParam 0 1 Unboxed [])
+         $ Map.insert "Bool"  (TyConParam 0 2 Boxed ["B"]) -- data Bool = B Bool#
+         $ Map.insert "Int"   (TyConParam 0 3 Boxed ["I"]) -- data Int = I Int#
+         $ Map.insert "List"  (TyConParam 1 4 Boxed ["Nil","Cons"]) -- data List a = Nil | Cons a (List a)
            Map.empty
 
 -- starting datacon map
 -- these tags must match what is in stg_header.h
 dataconmap :: DataConMap
-dataconmap = Map.insert "False_h" (DataConParam 0 0 False "Bool#") 
-           $ Map.insert "True_h"  (DataConParam 0 1 False "Bool#") 
-           $ Map.insert "B"     (DataConParam 1 2 True "Bool") 
-           $ Map.insert "I"     (DataConParam 1 3 True "Int") 
-           $ Map.insert "Unit"  (DataConParam 0 4 True "Unit")
-           $ Map.insert "Nil"   (DataConParam 0 5 True "List") 
-           $ Map.insert "Cons"  (DataConParam 2 6 True "List")
+dataconmap = Map.insert "False_h" (DataConParam 0 0 Unboxed "Bool#") 
+           $ Map.insert "True_h"  (DataConParam 0 1 Unboxed "Bool#") 
+           $ Map.insert "B"     (DataConParam 1 2 Boxed "Bool") 
+           $ Map.insert "I"     (DataConParam 1 3 Boxed "Int") 
+           $ Map.insert "Unit"  (DataConParam 0 4 Boxed "Unit")
+           $ Map.insert "Nil"   (DataConParam 0 5 Boxed "List") 
+           $ Map.insert "Cons"  (DataConParam 2 6 Boxed "List")
              Map.empty
+
+buildconmaps :: [TyCon] -> ConMaps
+buildconmaps objs = execState (build [] objs) 
+                    (tyconmap, dataconmap)
+                    
+                    
+updateTycons :: [TyCon] -> ([TyCon], ConMaps)                   
+updateTycons inp = (update (fst conmaps) inp, conmaps)
+                 where conmaps = buildconmaps inp
+
+class BuildConMaps t where build :: String -> t -> State ConMaps()
+                                                    
+instance BuildConMaps a => BuildConMaps [a] where
+  build = mapM_ . build
+
+instance BuildConMaps TyCon where
+  build _ (TyCon boxed con tyvars dcons) 
+    = tyconinsert con (length tyvars) boxed >> 
+      build con dcons
+     
+instance BuildConMaps DataCon where
+  build tycon (DataCon boxed con mts) = 
+    dataconinsert con (length mts) boxed tycon
+  
+dataconinsert :: String -> Int -> Boxedness -> Con -> State ConMaps ()
+dataconinsert con arity boxed tycon
+  = do 
+      (tmap, dmap) <- get
+      let dmap' = Map.insert con DataConParam{darity = arity, 
+                                             dtag = Map.size dmap, 
+                                             dboxed = boxed, 
+                                             dtycon = tycon} 
+                                             dmap
+      -- update tycon w/ this data con                                       
+      let Just (TyConParam {tarity, ttag, tboxed, tdatacons}) = Map.lookup tycon tmap                                        
+      let tmap' = Map.insert tycon TyConParam{tarity = tarity, 
+                                     ttag = ttag, 
+                                     tboxed = tboxed,
+                                     tdatacons = (con:tdatacons)}
+                                     tmap
+      put(tmap',dmap')
+    
+    
+tyconinsert :: String -> Int -> Boxedness -> State ConMaps ()
+tyconinsert con arity boxed 
+  = do 
+      (tmap, dmap) <- get
+      put (Map.insert con TyConParam{tarity = arity, 
+                                     ttag = Map.size tmap, 
+                                     tboxed = boxed,
+                                     tdatacons = []}
+                                     tmap, dmap)        
+                   
+
+-- update Atype -> Boxed/Unboxed 
+class Update t where update :: TyConMap -> t -> t              
+
+instance Update a => Update [a] where
+  update = map . update
+
+instance Update (Def a) where
+  update m (DataDef tycon) = DataDef(update m tycon)
+  update _ x  = x
+
+instance Update TyCon where
+  update m (TyCon boxed c tvs dcs) = 
+    TyCon boxed c tvs (update m dcs)
+ 
+instance Update DataCon where 
+  update m (DataCon boxed c mts) = 
+    DataCon boxed c (update m mts)
+
+instance Update Monotype where
+  update m (Mono ty) = if isboxed m ty == Boxed
+                       then MBoxed (makeboxed m ty) 
+                       else MUnboxed (makeunboxed m ty)
+  update _ ty = ty -- type is already boxed/unboxed, shoud never happen
+
+makeboxed :: TyConMap -> Atype -> Boxedtype
+makeboxed _ (ATyVar x) = BTyVar x
+makeboxed _ (AInt) = error "can't convert Int# to boxed"
+makeboxed _ (ADouble) = error "can't convert Double# to boxed"
+makeboxed m (AFun x y) = BFun (update m (Mono x)) (update m (Mono y))
+makeboxed m (ATyCon c xs) = BTyCon c (map (makeboxed m) xs)
+
+makeunboxed :: TyConMap -> Atype -> Unboxedtype
+makeunboxed _ (ATyVar a) = error ("can't convert " ++ a ++ " to unboxed")
+makeunboxed _ (AInt) = UInt
+makeunboxed _ (ADouble) = UDouble
+makeunboxed _ (AFun x y) = error ("can't convert function "
+                         ++ show x ++ " -> " ++ show y ++ " to unboxed")
+makeunboxed m (ATyCon c xs) = UTyCon c (map (makeboxed m) xs)
+  
+isboxed :: TyConMap -> Atype -> Boxedness  
+isboxed _ (ATyVar _) = Boxed
+isboxed _ (AInt) = Unboxed
+isboxed _ (ADouble) = Unboxed
+isboxed _ (AFun _ _) = Boxed
+isboxed m (ATyCon c _) = let lookup = Map.lookup c m
+                           in if (isNothing lookup) then 
+                                error ("unknown type constructor " ++ c ++ " used")
+                              else (\(TyConParam{tboxed=b}) -> b) (fromJust lookup)
+
--- codegen/stgc.hs
+++ codegen/stgc.hs
@@ -1,11 +1,84 @@
--- stgc "tests/Prelude.stg"
+{-# LANGUAGE NamedFieldPuns    #-}
+
 import Parser
 import Driver
 
 import Data.List
+import System.Console.GetOpt
 import System.Environment
 import System.IO
+import System.Process
+import Data.Maybe
 
+data Options = Options
+    { optVerbose     :: Bool
+    , optDumpParse   :: Bool
+    , optNoPrelude   :: Bool
+    , optOutput      :: Maybe FilePath
+    , optInput       :: Maybe FilePath
+    } deriving Show
+
+defaultOptions       = Options
+    { optVerbose     = False
+    , optDumpParse   = False
+    , optNoPrelude   = False
+    , optInput       = Nothing
+    , optOutput      = Just "a.out"
+    }
+
+--options :: [OptDescr (Options -> Options)]
+options =
+    [ Option ['v']     ["verbose"]
+        (NoArg (\ opts -> opts { optVerbose = True }))
+        "debug output on stderr"
+    , Option ['d'] ["dump-parse"]
+        (NoArg (\ opts -> opts { optDumpParse = True }))
+        "parser output only"
+    , Option ['p'] ["no-prelude"]
+        (NoArg (\ opts -> opts { optNoPrelude = True }))
+        "do not include prelude"
+    , Option ['o']     ["output"]
+        (ReqArg ((\ f opts -> opts { optOutput = Just f })) "FILE")
+        "output FILE"
+    , Option ['c']     []
+        (ReqArg ((\ f opts -> opts { optInput = Just f })) "FILE")
+        "input FILE"
+    ]
+
+compilerOpts :: [String] -> IO (Options, [String])
+compilerOpts argv =
+      case getOpt Permute options argv of
+         (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
+         (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+      where header = "Usage: stgc [OPTION...] files..."
+
+checkOpts :: Options -> Options
+checkOpts (Options {optInput}) = error ""
+
+stgcc :: String -> IO()
+stgcc input = let update x = x {optInput = Just input}
+              in compile (update defaultOptions) "../driver" "../runtime"
+
+compile :: Options -> String -> String -> IO ()
+compile  (Options {optVerbose, optDumpParse, optNoPrelude, optOutput, optInput}) preludeDir runtimeDir = 
+  do 
+    let input = fromJust optInput
+    ifd <- openFile input ReadMode
+    src <- hGetContents ifd
+    pfd <- openFile (preludeDir ++ "/Prelude.stg") ReadMode
+    prelude <- hGetContents pfd
+    let source = if optNoPrelude then src 
+                 else prelude ++ src 
+                 
+    case optDumpParse of
+      True  -> writeFile (input ++ ".dump") (show $ parse source)
+      False -> do 
+                 let coutput = input ++ ".c"
+                 let flags = " -std=gnu99 -L" ++ runtimeDir ++ " -I" ++ runtimeDir ++ " -lruntime"
+                 writeFile coutput (codegener source optVerbose)
+                 system ("gcc " ++ coutput ++ " -o " ++ (fromJust optOutput) ++ flags)
+                 return ()   
+              
 stgc :: String -> IO ()
 stgc arg =
   do
