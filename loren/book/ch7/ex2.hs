aeba3333c1347174e278d6c5cc3e40e12bcd83e7
Loren Anderson <loren@lanl.gov>
2015-09-11 10:46:32 -0600

    adding files

--- loren/book/ch7/ex2.hs
+++ loren/book/ch7/ex2.hs
@@ -15,3 +15,33 @@ dropWhile1 f xs | null xs = []
 dropWhile1 f xs | otherwise = if f (head xs)
                               then dropWhile1 f (tail xs) else xs
 
+all2 :: (a -> Bool) -> [a] -> Bool
+all2 f [] = True
+all2 f (x:xs) = if f x then all2 f xs else False
+
+any2 :: (a -> Bool) -> [a] -> Bool
+any2 f [] = False
+any2 f (x:xs) = if f x then True else any2 f xs
+
+takeWhile2 :: (a -> Bool) -> [a] -> [a]
+takeWhile2 f xs | null xs   = []
+                | otherwise = if f (head xs)
+                                  then (head xs):(takeWhile1 f (tail xs))
+                                             else []
+
+dropWhile2 :: (a -> Bool) -> [a] -> [a] 
+dropWhile2 f xs | null xs   = []
+                | otherwise = if f (head xs)
+                              then dropWhile1 f (tail xs) else xs
+
+takeWhile3 :: (a -> Bool) -> [a] -> [a]
+takeWhile3 f xs = take n xs 
+                      where n = findTake f 0 xs (length xs)
+
+findTake :: (a -> Bool)-> Int -> [a] -> Int -> Int
+findTake f n xs k | n==k = k
+                | otherwise = if f (xs !! n) then findTake f (n+1) xs k else n 
+
+dropWhile3 :: (a -> Bool) -> [a] -> [a]
+dropWhile3 f xs = drop n xs 
+                      where n = findTake f 0 xs (length xs)
