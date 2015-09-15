f7a239edf36561de1ee25e16ef69aab414735be1
Loren Anderson <loren@lanl.gov>
2015-09-15 11:06:53 -0600

    updating ch6 exercises

--- loren/book/ch6/ex3.hs
+++ loren/book/ch6/ex3.hs
@@ -18,10 +18,14 @@ elem1 :: Eq a => a -> [a] -> Bool
 elem1 a [] = False
 elem1 a (x:xs) = if x==a then True else elem1 a xs
 
---New Concat Code
 concat2 :: [[a]] -> [a]
 concat2 [[]] = []
 concat2 (xs:css) | null xs = concat2 css
 concat2 ((x:xs):css) = x:(concat2 (xs:css))
---End New Concat Code
 
+--New Concat Code
+concat3 :: [[a]] -> [a]
+concat3 [] = []
+concat3 ([]:css) = concat3 css
+concat3 ((x:xs):css) = x:(concat3 (xs:css))
+--End New Concat Code
