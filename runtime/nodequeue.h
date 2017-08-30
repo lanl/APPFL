16d9ceafaa18e378619d3c2c7b87af82532ae895
dpx <dpx@lanl.gov>
2017-08-30 14:40:54 -0600

    cleanup

--- runtime/nodequeue.h
+++ runtime/nodequeue.h
@@ -1,22 +1,15 @@
 // non-locking node queue
 
-#ifndef NODEQUEUE_H
-#define NODEQUEUE_H
+#ifndef QUEUE_H
+#define QUEUE_H
 
-#include "nodepool.h"
 #include <stdint.h>
 #include <stdbool.h>
 
-// lock free queue is broken use lock...
-// use lock on queue w/ clang for now
-#if defined(__clang__)
-#define USE_LOCK 0 
-#else
-#define USE_LOCK 0
-#endif
+typedef uintptr_t T;
 
 void NQ_init();
 void NQ_enqueue(T value);
 bool NQ_dequeue(T *value);
 
-#endif // NODEQUEUE_H
+#endif // QUEUE_H
